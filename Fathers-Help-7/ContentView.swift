9ca156c2446fbf7b0b5d8a807810ba1118f809e7
umpu <carol.ledgers0h@icloud.com>
2024-03-17 21:41:48 +0500

    Initial Commit

---
+++ Fathers-Help-7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fathers-Help-7
+//
+//  Created by a on 17.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fathers-Help-7/Fathers_Help_7App.swift
@@ -0,0 +1,17 @@
+//
+//  Fathers_Help_7App.swift
+//  Fathers-Help-7
+//
+//  Created by a on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Fathers_Help_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
